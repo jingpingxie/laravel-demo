3e74283abd10506fc1175b5805be274e5aceb26c
jason xie <jason.xie@95isee.com>
2022-05-31 09:02:10 +0800

    create

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Author jason.xie
+ * @Date 2022/5/31 8:43
+ */
+
+namespace App\Http\Controllers;
+
+
+class TasksController extends Controller
+{
+    public function home()
+    {
+        return "Hello,World!";
+    }
+
+}
